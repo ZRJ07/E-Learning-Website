5551fd6a1ac973f3a804b0d6e0ba8d21b8344462
Oahid Zihad <32256716+OahidZihad@users.noreply.github.com>
2019-12-23 01:39:50 +0600

    update views.py

--- educa/students/views.py
+++ educa/students/views.py
@@ -47,6 +47,20 @@ class StudentRegistrationView(CreateView):
         login(self.request, user)
         return result
 
+# to view the course enroll page
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super(StudentEnrollCourseView,
+                     self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail',
+                            args=[self.course.id])
 
 
 =======
